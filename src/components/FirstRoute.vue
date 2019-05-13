d34259318a0edd224e9ed48a250a557d8528b294
TD <a6209123456789@yahoo.com.tw>
2019-05-13 17:29:04 +0800

    vue router test

---
+++ src/components/FirstRoute.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+  <div>{{ msg  }}</div>
+  hello {{ $route.params.name }}
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstRoute',
+  data () {
+    return {
+      msg: 'FirstRoute'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
